db18fa552f1ac0b627aeaba0dc0bfa58a5094c46
Abhi Nemani <abhi.nemani@gmail.com>
2010-12-22 21:20:38 -0800

    started blog page

--- single.php
+++ single.php
@@ -9,28 +9,37 @@
 
 get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+	<div class="wrap clearfix" id="inner">
+	  <div id="maincontent">
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<div id="nav-above" class="navigation">
+			<!--	<div id="nav-above" class="navigation">
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
-				</div><!-- #nav-above -->
+				</div>--><!-- #nav-above -->
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
+					 <div class="ttl"><?php the_title(); ?><a href="#" class="red-btn"><span>Share This +</span></a></div>
 
-					<div class="entry-meta">
-						<?php twentyten_posted_on(); ?>
+					<div class="entry-meta" style="margin-top: 10px;">
+					<small>	<?php twentyten_posted_on(); ?></small>
 					</div><!-- .entry-meta -->
 
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->
-
+					</div>
+						<div class="entry-utility">
+							<?php twentyten_posted_in(); ?>
+							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-utility --><!-- .entry-content -->
+						
+						<div id="nav-below" class="navigation">
+							<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
+							<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
+						</div><!-- #nav-below -->
+						<?php comments_template( '', true ); ?>
+</div>
 <?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
 					<div id="entry-author-info">
 						<div id="author-avatar">
@@ -48,23 +57,11 @@ get_header(); ?>
 					</div><!-- #entry-author-info -->
 <?php endif; ?>
 
-					<div class="entry-utility">
-						<?php twentyten_posted_in(); ?>
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-utility -->
-				</div><!-- #post-## -->
-
-				<div id="nav-below" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
-				</div><!-- #nav-below -->
-
-				<?php comments_template( '', true ); ?>
+				
 
 <?php endwhile; // end of the loop. ?>
 
-			</div><!-- #content -->
-		</div><!-- #container -->
+		
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
