ee1cdaf76d51a191384f69c8ecc044fce0ea2dbf
Dan Melton <melton.dan@gmail.com>
2010-12-30 15:00:26 -0800

    Added fellow carousel

---
+++ fellow_carousel.php
@@ -0,0 +1,29 @@
+<div id="fellows">
+	<h3 class="ttl">Meet a Fellow <a href="#" class="more">Apply for the fellowship</a></h3>
+	<div id="fellowcarousel">
+        <ul>
+          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>1</strong></span></li>
+          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>2</strong></span></li>
+          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>3</strong></span></li>
+          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>4</strong></span></li>  
+          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>5</strong></span></li>
+          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>6</strong></span></li>
+          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>7</strong></span></li>
+          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>8</strong></span></li>  
+		  <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>9</strong></span></li>
+          <li><a href="http://www.esri.com" title="ESRI"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>10</strong></span></li>                
+          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>1</strong></span></li>
+          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>2</strong></span></li>
+          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>3</strong></span></li>
+          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>4</strong></span></li>  
+          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>5</strong></span></li>
+          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>6</strong></span></li>
+          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>7</strong></span></li>
+          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>8</strong></span></li>  
+		  <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>9</strong></span></li>
+          <li><a href="http://www.esri.com" title="ESRI"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>10</strong></span></li>          
+</ul>             				
+     </div>
+     <div class="prev"><img src="<?php bloginfo('template_directory'); ?>/images/prev-arrow.gif"></div>
+     <div class="next"><img src="<?php bloginfo('template_directory'); ?>/images/next-arrow.gif"></div>
+</div>
