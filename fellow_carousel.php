1bf50c3498b9d4fde286b68d1c7817c0aa7ef073
Dan Melton <melton.dan@gmail.com>
2010-12-30 15:18:38 -0800

    Added fellow carousel

--- fellow_carousel.php
+++ fellow_carousel.php
@@ -2,26 +2,26 @@
 	<h3 class="ttl">Meet a Fellow <a href="#" class="more">Apply for the fellowship</a></h3>
 	<div id="fellowcarousel">
         <ul>
-          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>1</strong></span></li>
-          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>2</strong></span></li>
-          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>3</strong></span></li>
-          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>4</strong></span></li>  
-          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>5</strong></span></li>
-          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>6</strong></span></li>
-          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>7</strong></span></li>
-          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>8</strong></span></li>  
-		  <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>9</strong></span></li>
-          <li><a href="http://www.esri.com" title="ESRI"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>10</strong></span></li>                
-          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>1</strong></span></li>
-          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>2</strong></span></li>
-          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>3</strong></span></li>
-          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>4</strong></span></li>  
-          <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>5</strong></span></li>
-          <li><a href="http://www.omidyar.com/" title="Omidyar Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>6</strong></span></li>
-          <li><a href="http://www.rockefellerfoundation.org/" title="Rockefeller Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>7</strong></span></li>
-          <li><a href="http://www.casefoundation.org" title="Case Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>8</strong></span></li>  
-		  <li><a href="http://www.sunlightfoundation.org" title="Sunlight Foundation"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>9</strong></span></li>
-          <li><a href="http://www.esri.com" title="ESRI"><img src="<?php bloginfo('template_directory'); ?>/images/fellow.jpg"></a><span><strong>10</strong></span></li>          
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/bernstein.png"></a><span><strong>Michael Bernstein</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/bloom.png"></a><span><strong>Anna Bloom</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/canfield.png"></a><span><strong>Jemery Canfield</strong></span></li>  
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/evans.png"></a><span><strong>Michael Evans</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/fecteau.png"></a><span><strong>Peter Fecteau</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/koeth.png"></a><span><strong>Michelle Koeth</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/lewis.png"></a><span><strong>Michael Lewis</strong></span></li>  
+		  <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/macedo.png"></a><span><strong>Karla Macedo</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/mahoney.png"></a><span><strong>Joel Mahoney</strong></span></li>                
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/mertens.png"></a><span><strong>John Mertens</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/ober.png"></a><span><strong>Erik Michaels-Ober</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/ogden.png"></a><span><strong>Max Ogden</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/ogle.png"></a><span><strong>Aaron Ogle</strong></span></li>  
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/palazzolo.png"></a><span><strong>Alan Palazzolo</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/poe.png"></a><span><strong>Mjumbe Poe</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/resella.png"></a><span><strong>Ryan Resella</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/salway.png"></a><span><strong>Talin Salway</strong></span></li>  
+		  <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/sikes.png"></a><span><strong>Chacha Sikes</strong></span></li>
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/silverman.png"></a><span><strong>Scott Silverman</strong></span></li>          
+          <li><a href="" title=""><img src="<?php bloginfo('template_directory'); ?>/images/2011fellows/stalder.png"></a><span><strong>Tyler Stalder</strong></span></li>          
 </ul>             				
      </div>
      <div class="prev"><img src="<?php bloginfo('template_directory'); ?>/images/prev-arrow.gif"></div>
