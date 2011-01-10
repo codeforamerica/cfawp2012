0c991a341667d024d974c2cb6173f698671a7d81
Abhi Nemani <abhi.nemani@gmail.com>
2011-01-10 13:29:11 -0800

    updated single page

--- single.php
+++ single.php
@@ -34,7 +34,6 @@ get_header(); ?>
 							<!--<?php twentyten_posted_in(); ?>-->
 							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
 						</div><!-- .entry-utility --><!-- .entry-content -->
-					<hr />	
 						<!--<div id="nav-below" class="navigation">
 							<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
 							<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
