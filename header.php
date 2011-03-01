0674ad37baa04dfcbce045d3326973b549787010
Abhi Nemani <abhi.nemani@gmail.com>
2011-03-01 11:56:36 -0800

    updated share button

--- header.php
+++ header.php
@@ -120,7 +120,7 @@ $(document).ready(function() {
 	wp_head();
 ?>
 
-<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript">var switchTo5x=true;</script><script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script><script type="text/javascript">stLight.options({publisher:'72c27113-3bfc-4c68-a34e-b85a85d478db'});</script>
 </head>
 
 
