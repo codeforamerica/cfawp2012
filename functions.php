e2f59a2b9c13c70ba32ead3e2072f4ba0e62636f
Abhi Nemani <abhi.nemani@gmail.com>
2011-03-07 11:53:02 -0800

    updated index

--- functions.php
+++ functions.php
@@ -440,8 +440,8 @@ function twentyten_widgets_init() {
 		'name' => __( 'Third Footer Widget Area', 'twentyten' ),
 		'id' => 'third-footer-widget-area',
 		'description' => __( 'The third footer widget area', 'twentyten' ),
-		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-		'after_widget' => '</li>',
+		'before_widget' => '',
+		'after_widget' => '',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
