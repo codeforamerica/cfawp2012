2c33e6869d5ed36cb1bbc3bef3ee0f474ee21948
Dan Melton <melton.dan@gmail.com>
2011-03-10 08:38:32 -0800

    Altered project templates

--- projects-template.php
+++ projects-template.php
@@ -23,12 +23,14 @@ get_header(); ?>
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
           
       <?php the_content(); ?><?php endwhile; ?>
-      <h4>Tools and Resources</h4>
+      <h4>Our Tools and Resources</h4>
 <ul>
+    <li><a href="http://groups.google.com/group/cfa-labs">Join the Main Google Group</a></li>
     <li><a href="http://help.github.com/">Learn How to Setup Git</a></li>
 	<li><a href="http://github.com/codeforamerica">Code for America GitHub</a></li>    
     <li><a href="http://devcenter.heroku.com/articles/quickstart">Learn How to Setup Heroku</a></li>
     <li><a href="http://rubyonrails.org/download">Learn How to Setup Rails</a></li>
+    <li><a href="http://www.pivotaltracker.com/features">Learn About Pivotal Tracker</a></li>    
 </ul>
       
     </div>
