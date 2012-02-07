637de9b3676805927746cb62b7896186a4a6f884
Aurelio Tinio <aurelio@tinio.org>
2012-02-06 22:49:13 -0600

    austin: add mailchimp signup form

--- austin-template.php
+++ austin-template.php
@@ -16,8 +16,24 @@ get_header(); ?>
 <div class="wrap clearfix">
   <div id="mission">
     <h2>Team Austin loves cookies!</h2>
-    <p>Sign up for our newsletter below.</p>
-		<input></input>
+    <p>
+			<!-- Begin MailChimp Signup Form -->
+			<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+			<style type="text/css">
+				#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+				/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+				   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+			</style>
+			<div id="mc_embed_signup">
+			<form action="http://codeforamerica.us4.list-manage1.com/subscribe/post?u=7eae25b3fcdc6e862a1a20456&amp;id=5fa034319d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+				<label for="mce-EMAIL">Subscribe to our mailing list</label>
+				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			</form>
+			</div>
+
+			<!--End mc_embed_signup-->    	
+    </p>
   </div>
   <img src="<?php bloginfo( 'template_url' ); ?>/images/austin/austin_header_1.gif" alt="" />
 </div>
@@ -26,15 +42,18 @@ get_header(); ?>
 
 <div class="wrap clearfix" id="fellows-wrap">
   <div id="maincontent">
-    <h2 class="ttl">Why Become a Fellow? <a href="#" class="red-btn"><span class="st_sharethis_custom">SHARE THIS +</span></a></h2>
+		
+    <h2 class="ttl">Our Goals <a href="#" class="red-btn"><span class="st_sharethis_custom">SHARE THIS +</span></a></h2>
 <p>	During the 11-month program, fellows not only work together to help cities innovate, but also receive the training and support to be positioned as a leader in business, public service, or both:</p>
     <ul id="why">
-      <li><strong class="number">1</strong> <strong>Gov 2.0 Training.</strong> You will start with a crash course in municipal government and gain a practical understanding of the vital intersection of politics and technology.</li>
-      <li><strong class="number">2</strong> <strong>Professional Development.</strong> The biggest names in the web industry and the Gov 2.0 movement will provide you unparalleled networking, mentoring, and support.</li>
-      <li><strong class="number">3</strong> <strong>Alumni Connections.</strong> Working closely with other talented individuals, you will develop lasting professional relationships in a fast-paced, startup atmosphere.</li>
+      <li><strong class="number">1</strong> <strong>Open data</strong> You will start with a crash course in municipal government and gain a practical understanding of the vital intersection of politics and technology.</li>
+      <li><strong class="number">2</strong> <strong>Understand our audience</strong> The biggest names in the web industry and the Gov 2.0 movement will provide you unparalleled networking, mentoring, and support.</li>
+      <li><strong class="number">3</strong> <strong>Nurture civic hackers</strong> Working closely with other talented individuals, you will develop lasting professional relationships in a fast-paced, startup atmosphere.</li>
+      <li><strong class="number">4</strong> <strong>Identify apps and projects</strong> Working closely with other talented individuals, you will develop lasting professional relationships in a fast-paced, startup atmosphere.</li>
     </ul>
+		
     <div class="box">
-      <h3 class="ttl">What Are We looking For? <a href="/fellows/2012-program-and-timeline/" class="more">The Program</a></h3>
+      <h3 class="ttl">What Are We looking For? <a href="/fellows/2012-program-and-timeline/" class="more">Read More</a></h3>
       <ul id="what" class="clearfix">
         <li><img src="<?php bloginfo( 'template_url' ); ?>/images/fellows1.jpg" alt="" /></li>
         <li><img src="<?php bloginfo( 'template_url' ); ?>/images/fellows2.jpg" alt="" /></li>
@@ -43,9 +62,16 @@ get_header(); ?>
       </ul>
       <p>Anyone with the skills and passion to make cities work better using technology. Many of you will be starting your careers, but others of you will have been working on the web for years and just want a chance to do something important and altruistic. All of you will want to see your work make a difference to cities and change how government works.</p>
     </div>
+		
     <div class="box">
-      <h3 class="ttl">The application Process <a href="/fellows/fellow-selection/" class="more">Read More</a></h3>
-      <p>To apply, prospective fellows must submit examples of previous relevant work, along with short essays explaining their interest, goals, and background. The 2012 Fellowship begins in January 2012. The application cycle for 2013 is now open with a <strong>Spring Cycle deadline of March 18, 2012</strong>.</p>
+      <h3 class="ttl">Why Austin? <a href="/2012-city-finalists/austin-old/" class="more">Read More</a></h3>
+			<p id="internal-source-marker_0.628739543234031"><a href="http://codeforamerica.org/wp-content/uploads/2011/06/austinv3.jpeg"><img class="aligncenter size-full wp-image-6290" title="austinv3" src="http://codeforamerica.org/wp-content/uploads/2011/06/austinv3.jpeg" alt="" width="630" height="180" /></a></p>
+
+			<blockquote>“I  think it would be good if Austin could work with Code for America  fellows and create new applications for our community... I know  Austin’s tech community will be very interested in the Fellowship  program – it’s a great opportunity.”
+
+			- Mayor Lee Leffingwell</blockquote>
+			<a href="http://codeforamerica.org/wp-content/uploads/2011/06/Austinseal1.jpg"><img class="alignright size-medium wp-image-6293" title="Austinseal" src="http://codeforamerica.org/wp-content/uploads/2011/06/Austinseal1-300x300.jpg" alt="" width="168" height="168" /></a>Austin  has proposed to partner with Code for America to produce lightweight  creative technology that empowers the city to become a platform for  community engagement. The project will extend access to government that  allows city agencies to easily and effectively deliver information and  services. Instead of a citizen government relationship limited to  request and response, such a system will allow for more dynamic  collection of feedback, issue management, email notification, discussion  and event management.			
+			The  goals of transparency and openness are becoming standard expectations  in nearly every level of government. Often government agencies struggle  to achieve these goals and meet the demands of the communities they  serve due to a lack of technological resources that make common  information and services available.  The City of Austin is challenged  with keeping up with their motivated and engaged community. City leaders  are committed to closing the gap between typical city government issues  and the best impulses of the community it represents.
     </div>
   </div>
   <div class="right-col">
