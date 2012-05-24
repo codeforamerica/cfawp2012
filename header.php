5515aeb44fa664e479b213075c5417f8e6d45e6a
Abhi <abhi.nemani@gmail.com>
2012-05-24 09:53:26 -0700

    swapped apply for get involved

--- header.php
+++ header.php
@@ -159,7 +159,7 @@ $(document).ready(function() {
 	<!--Navigation -->
 	<div id="nav-wrap">
 		<?php wp_nav_menu( array('menu'=> 'cfa_main', 'menu_id' => 'nav', 'container' => 'ul',  ) ); ?>
-		<a href="/apply" id="donate" class="like-btn">Apply</a>
+		<a href="/get-involved/" id="donate" class="like-btn">Get Involved</a>
 	</div>
 </div>
 	
