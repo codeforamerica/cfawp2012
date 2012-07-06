da13aa8197eb9935926e229f7ee4a94ec39732c5
Abhi <abhi.nemani@gmail.com>
2012-07-06 10:59:21 -0700

    updated homepage

--- index.php
+++ index.php
@@ -24,11 +24,11 @@ get_header(); ?>
 		    height: 315px;
 		">
 		<div class="featured-home-img">
-			<a href="http://codeforamerica.org/2012/03/08/jennifer-pahlka-at-ted-video/"><img src="http://codeforamerica.org/wp-content/uploads/2012/04/jented.png" align="right" style="height: 250px; padding: 30px 40px;"></a>
+			<a href="http://codeforamerica.org/apply"><img src="http://codeforamerica.org/wp-content/uploads/2012/07/biz2.png" align="right" style="height: 225px; padding: 30px 40px;"></a>
 		</div>
 		<div class="callout" style="width: 450px; padding: 40px;">
-      <h2 style="font-family: oswald; text-transform: uppercase; color: black; font-size: 30px; text-align: left; letter-spacing: -.04em;.; line-height: 1.2em;">"We Need to Remember We're Not Just Consumers. We're Citizens"</h2>
-      <p style="text-align: left; padding-left: 3px;">CfA's Founder Jennifer Pahlka gave a TED Talk about our fellowship program, which calls technologists into public service.<br /> We're now recruiting our 2013 class. <strong>Make a difference.</strong></p>
+      <h2 style="font-family: oswald; text-transform: uppercase; color: black; font-size: 30px; text-align: left; letter-spacing: -.04em;.; line-height: 1.2em;">Code the Next Chapter of American History</h2>
+      <p style="text-align: left; padding-left: 3px;">If you're a developer, designer, or entrepreneur, the CfA Fellowship is your opportunity to use your skills to make a difference.<br /><strong>The deadline for applications is July 29. </strong></p>
       <a href="/apply"><img src="http://codeforamerica.org/wp-content/uploads/2012/04/apply.png" align="left"></a>
       <a href="/fellows"><img src="http://codeforamerica.org/wp-content/uploads/2012/03/learn.png" align="left"></a></div>
 		</div>
