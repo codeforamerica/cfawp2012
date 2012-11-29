eb351e1dbccf289937665b8101b181333f5afe74
Abhi Nemani <abhi.nemani@gmail.com>
2012-11-29 09:50:32 -0800

    changed homepage to video

--- index.php
+++ index.php
@@ -18,16 +18,18 @@ get_header(); ?>
   <div class="wrap-b">
 	  <!--featured -->	  
 	
-    <div id="featured" style="width: 935px; margin: 0 auto; height: 310px; background: url(http://codeforamerica.org/wp-content/uploads/2012/10/cities-back.png) no-repeat bottom;">
-    		  <div class="callout" style="width: 450px; padding: 50px 50px 105px 160px;">
-    <h1 style="font-family: 'Oswald', Helvetica; text-transform: uppercase; color: black; font-size: 40px; letter-spacing: -.05em; text-align: left;">The CfA Fellowship</h1>
-    		    <p style="text-align: left; padding-left: 3px;padding-top: 0px;margin-top: 0px;">Code for America partners with innovative city leaders, looking for a different way of doing things. Your city could be next:</p>
-    		    <a href="/2013-partners" data-bitly-type="bitly_hover_card" style=""><img src="http://codeforamerica.org/wp-content/uploads/2012/10/2013.png" align="right"></a>
-    		    <a href="/cities/apply-now/" data-bitly-type="bitly_hover_card"><img src="http://codeforamerica.org/wp-content/uploads/2012/10/more.png" align="right"></a>
-    		  </div>
-    		</div>
+      <div id="featured" style="width: 935px; margin: 0 auto; height: 290px; background: url() no-repeat bottom;padding: 20px 0px;">
+          		   		  <div class="callout" style="width: 400px; padding: 50px 10px 105px 0px; float: left;">
+          <h1 style="font-family: 'Oswald', Helvetica; text-transform: uppercase; color: black; font-size: 40px; letter-spacing: -.05em; text-align: left;">The CfA Fellowship</h1>
+          		    <p style="text-align: left; padding-left: 3px;padding-top: 0px;margin-top: 0px;">Code for America partners with innovative city leaders, looking for a different way of doing things. Your city could be next:</p>
+          		    <a href="/cities/apply-now/" data-bitly-type="bitly_hover_card"><img src="http://codeforamerica.org/wp-content/uploads/2012/10/more.png" align="left"></a>
+          		    <a href="/2013-partners" data-bitly-type="bitly_hover_card" style=""><img src="http://codeforamerica.org/wp-content/uploads/2012/10/2013.png" align="left"></a>
+          		  </div>
+          		  <div id="map" style="width: 500px; height: 260px; float: right; padding-top: 10px;">
+          		      <iframe src="http://player.vimeo.com/video/53640839" width="500" height="260" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+          		  </div>
 
-  </div>
+          		</div>
   
 	  <!--Newsletter Subscribtion -->
 
