0acca2941fedcc4d71d34814fc309164d805c8c9
akit <angel.kittiyachavalit@gmail.com>
2013-02-19 18:22:50 -0800

    Changed the top section of the cities page

--- city-all-template.php
+++ city-all-template.php
@@ -20,22 +20,19 @@ get_header(); ?>
 <div class="wrap clearfix" id="cities">
   <!--PARTICIPATING CITIES -->
   <div id="participating-wrap" class="clearfix">
-    <div id="participating">
-      <h2>CITY PARTNERS </h2>
-      <div class="links"> 2011: <a href="/boston">Boston <span>&raquo;</span></a> <a href="/philadelphia">Philadelphia <span>&raquo;</span></a> <a href="/seattle">Seattle <span>&raquo;</span></a> <br />
-	2012: <a href="/austin">Austin <span>&raquo;</span></a> <a href="/2012-partners/detroit">Detroit <span>&raquo;</span></a> <a href="/2012-partners/chicago">Chicago <span>&raquo;</span></a>  <a href="/2012-partners/honolulu">Honolulu <span>&raquo;</span></a>  <a href="/2012-partners/macon">Macon <span>&raquo;</span></a> <a href="/2012-partners/nola">New Orleans <span>&raquo;</span></a>  <a href="/2012-partners/philadelphia">Philadelphia <span>&raquo;</span></a>  <a href="/2012-partners/santa-cruz">Santa Cruz <span>&raquo;</span></a> <a href="http://codeforamerica.org/cities/apply/">Apply for 2014 <span>&raquo;</span></a></div>
-      <p>CfA connects forward-thinking cities with the talent from the web industry to develop reusable civic technology. Partnering cities will not only solve a critical problem using technology, but also help cultivate the next generation of tech-savvy, civic leaders.
-	
-	</p>
-      <ul>
-        <li><a href="/cities/faqs-cities-applying-to-program/">Learn more about the  program</a></li>
-        <li><a href="/citizen-action-center">Spread the word in your city</a></li>
-        <li><a href="http://codeforamerica.org/cities/apply/" class="active">apply to become a cfa city</a></li>
-      </ul>
+
+    <div class="ttl fullcolumn">
+      Bring CfA to your city
     </div>
-	<link rel="stylesheet" href="http://leaflet.cloudmade.com/dist/leaflet.css" />
-	<!--[if lte IE 8]><link rel="stylesheet" href="../dist/leaflet.ie.css" /><![endif]-->
-	<div id="map" style="width: 600px; height: 325px"><iframe src="http://player.vimeo.com/video/53640839" width="600" height="325" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
+
+    <div id="participating">
+      <p>We just opened up the application process for our 2014 City Fellowship Program, and we are actively seeking local governments who want to engage with our fellowship program. As many of you know, the Code for America Fellowship is a year long program focused on civic collaboration and innovation. 
+
+But don’t take our word for it. Find out what past partners have to say about the program. </p>
+      <p><a class="button" href="http://codeforamerica.org/cities/apply-now/">Apply Now<img src="<?php bloginfo( 'template_url' ); ?>/images/arrow_white.png" alt="arrow" /></a></p>
+      <p><strong>Applications due March 31, 2013</strong></p>
+    </div>
+	<div id="video" style="width: 600px; height: 325px"><iframe src="http://player.vimeo.com/video/53640839" width="600" height="325" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
   </div>
 
   <div id="twocols-container" class="clearfix">
