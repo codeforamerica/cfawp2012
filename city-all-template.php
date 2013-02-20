51a1ea26283ce16efae0ad2fe51532e6754dce0d
akit <angel.kittiyachavalit@gmail.com>
2013-02-19 23:32:24 -0800

    Added overview and dates in mind sections

--- city-all-template.php
+++ city-all-template.php
@@ -26,72 +26,74 @@ get_header(); ?>
     </div>
 
     <div id="participating">
-      <p>We just opened up the application process for our 2014 City Fellowship Program, and we are actively seeking local governments who want to engage with our fellowship program. As many of you know, the Code for America Fellowship is a year long program focused on civic collaboration and innovation. 
-
-But don’t take our word for it. Find out what past partners have to say about the program. </p>
-      <p><a class="button" href="http://codeforamerica.org/cities/apply-now/">Apply Now<img src="<?php bloginfo( 'template_url' ); ?>/images/arrow_white.png" alt="arrow" /></a></p>
+      <p>We just opened up the application process for our 2014 City Fellowship Program, and we are actively seeking local governments who want to engage with our fellowship program. As many of you know, the Code for America Fellowship is a year long program focused on civic collaboration and innovation. But don’t take our word for it. Find out what past partners have to say about the program. </p>
+      <p><a class="button" href="http://codeforamerica.org/cities/apply-now/">Apply Now<img class="arrow" src="<?php bloginfo( 'template_url' ); ?>/images/arrow_white.png" alt="arrow" /></a></p>
       <p><strong>Applications due March 31, 2013</strong></p>
     </div>
 	<div id="video" style="width: 600px; height: 325px"><iframe src="http://player.vimeo.com/video/53640839" width="600" height="325" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
   </div>
 
-  <div id="twocols-container" class="clearfix">
-    <div id="maincontent" class="entry-content">
-      
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-
-								<?php the_content(); ?>
-							
-								<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-
-
-		<?php endwhile; ?>
-
-
+<div id="maincontent" class="entry-content">
+    <div class="left-col">
+      <h3>Overview</h3>
+      <p>The Fellowship partners talented teams of developers, designers, and entrepreneurs with change agents from local governments to address a range of civic challenges. Benefits to participation include:</p>
+      <ul>
+        <li><strong>Catalyst for innovation</strong></li>
+        <li><strong>New perspectives on issues your city is facing</strong></li>
+        <li><strong>Products &amp; interfaces that are simple, beautiful and easy to use</strong></li>
+        <li><strong>Collaboration with a network of civic innovators and thought leaders</strong></li>
+        <li><strong>Visibility on a national stage</strong></li>
+      </ul>
+      <p>The fellows commit to a year of service earning a living-wage stipend and move through the highly structured program using agile “Lean Startup” techniques to develop and implement web and mobile applications that show what’s possible, deeply engage citizens, and increase the transparency of government information and systems. Outcomes include increased civic engagement, strengthened relationships inside and outside of government, and technology tools and products targeted to a pressing issue your city is facing.</p>
+      <p>Know of another city doing great things? <a href="mailto:cityrecruitment@codeforamerica.org">Let us know</a>--we'll be sure to reach out to them?</p>
+      <a href="http://codeforamerica.org/cities/city-impact/">Learn more <span>&raquo;</span></a>
     </div>
-    <div class="right-col">
+    <div class="quote right-col">
+      <p>From Code for America we thought we’d just get some cool apps for our constituents, but we got so much more; new relationships in City Hall, new connections to the community, a platform for further innovation, and yes, even some great applications.</p>
+      <h4>Nigel Jacobs<br/>City of Boston</h4>
+    </div>
+    <div class="clearfix"></div>
+
+  <div id="twocols-container" class="clearfix">    
+
+    <div class="cityDates left-col">
       <h3>Dates To Keep In Mind</h3>
       <h4>2014 City Application Timeline</h4>
       <p>The application process for 2013 cities will be opening in early January. Till then, be sure to let us know your city is interested, and we can start the conversation.</p>
       <table border="0" cellspacing="0" cellpadding="0">
         <tr class="odd">
-          <td>1/09/13</td>
-          <th>Application Process<br />
-            Opened <span>&raquo;</span></th>
+          <td>January - March 2013</td>
+          <th>City Application Period</th>
         </tr>
         <tr>
-          <td>1/10 - 03/31</td>
-          <th>City Application Period <span>&raquo;</span></th>
+          <td>March 31, 2013</td>
+          <th>Deadline for Applications</th>
         </tr>
         <tr class="odd">
-          <td>03/31</td>
-          <th>Deadline for 
-            Applications <span>&raquo;</span></th>
+          <td>April 2013</td>
+          <th>City Applications Reviewed</th>
         </tr>
         <tr>
-          <td>4/1 - 4/30</td>
-          <th>City Applications Reviewed <span>&raquo;</span></th>
+          <td>May 2013</td>
+          <th>CfA meets with cities to confirm support &amp; funding</th>
         </tr>
         <tr class="odd">
-          <td>5/01 - 6/01</td>
-          <th>Cities confirm support & funding<span>&raquo;</span></th>
+          <td>June 1, 2013</td>
+          <th>City Finalists Announced (Rolling Acceptance)</th>
         </tr>
         <tr>
-          <td>6/01</td>
-          <th>City Finalists Announced (Rolling Acceptance)<span>&raquo;</span></th>
+          <td>October 2013</td>
+          <th>Selected Cities Announced to the Press</th>
         </tr>
         <tr class="odd">
-          <td>October</td>
-          <th>Selected Cities Announced to the Press <span>&raquo;</span></th>
-        </tr>
-        <tr>
           <td>January 2014</td>
-          <th>New Class of Fellows
-            Begins Program <span>&raquo;</span></th>
+          <th>New Class of Fellows Begin Program</th>
         </tr>
       </table>
+      <p><a class="button" href="http://codeforamerica.org/cities/apply-now/">Apply Now<img class="arrow" src="<?php bloginfo( 'template_url' ); ?>/images/arrow_white.png" alt="arrow" /></a></p>
     </div>
+
   </div>
 </div>
 <?php get_footer(); ?>
