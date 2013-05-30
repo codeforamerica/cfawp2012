6f90c550e4691e0757201efa6ab3c4453420d073
Abhi <abhi.nemani@gmail.com>
2013-05-30 09:57:20 -0700

    updated board

--- about-template.php
+++ about-template.php
@@ -28,18 +28,18 @@ get_header(); ?>
 	<div class="box">
 		<h3>Who we are</h3>
 		<p>Code for America is a new non-profit, and a new kind of organization. Our team is made up of web geeks, city experts, and technology industry leaders. We are building a network of civic leaders and organizations who believe there is a better way of doing things and want to make a difference.</p>
-		<p align="right"><a href="/who-we-are" class="more">Get to know us</a></p> 
+		<p align="right"><a href="/who-we-are" class="more">Our team</a></p> 
 	</div>
 	<div class="box">
 		<h3>what we do</h3>
 		<p>We make it easy and attractive for the web generation to give back through our Fellowship, which connects  technologists with cities to work together to innovate; our Accelerator, which will support disruptive civic startups; and our Brigade, which helps local, community groups reuse civic software.</p>
-		<p align="right"><a href="/cities" class="more">Our Current Projects</a></p> 
+		<p align="right"><a href="/programs" class="more">Our Programs</a></p> 
 		
 	</div>
 	<div class="box">
 			<h3>how we make a difference</h3>
 			<p>Code for America helps governments become more connected, lean, and participatory through new opportunities for public service -- both inside and outside government -- so we're not only making a direct impact everyday, but also creating the relationships and network for lasting change.</p>
-			<p align="right"><a href="/broader-impact" class="more">Our Broader Impact</a></p> 
+			<p align="right"><a href="/projects" class="more">Our Projects</a></p> 
 			
 	<!--	<h3>How we’re funded</h3>
 		<p>Code for America is generously supported by leaders in philanthropy and technology. Their support helps make our work connecting city government and web talent possible.</p>
@@ -58,8 +58,10 @@ get_header(); ?>
 		<li>	<a href="/who-we-are/#toreilly" rel="tooltip2" title="Tim O'Reilly"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/toreilly.jpg" alt="Staff name" /></a></li>
 		<li>	<a href="/who-we-are/#jlilly" rel="tooltip2" title="John Lilly"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/jlilly.jpg" alt="Staff name" /></a></li>
 		<li>	<a href="/who-we-are/#sdonohue" rel="tooltip2" title="Stacy Donohue"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/sdonohue.png" alt="Stacy Donohue" /></a></li>
-		<li>	<a href="/who-we-are/#eries" rel="tooltip2" title="Eric Ries"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/eries.png" alt="Staff name" /></a></li>
+		<li>	<a href="/who-we-are/#dthorman" rel="tooltip2" title="Damian Thorman"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/dthorman.png" alt="Staff name" /></a></li>
 		<li>	<a href="/who-we-are/#njacob" rel="tooltip2" title="Nigel Jacob"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/njacob.png" alt="Staff name" /></a></li>
+		<li class="first">	<a href="/who-we-are/#eries" rel="tooltip2" title="Eric Ries"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/eries.png" alt="Staff name" /></a></li>
+		
 		</ul>
 		
 		<h3>STAFF <span style="color: #999; margin-left: 10px;"><span id="tip1"></span></span></h3>
@@ -98,27 +100,7 @@ get_header(); ?>
 		</ul>
 
 
-		<h3>board of advisors <span style="color: #999; margin-left: 10px;"><span id="tip3"></span></span></h3>
-		<ul class="clearfix">
-				<li class="first"><a href="/who-we-are/#dbryant" rel="tooltip3" title="Deb Bryant"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/dbryant.jpg" alt="Staff name" /></a></li>
-				<li><a href="/who-we-are/#cjohnson" rel="tooltip3" title="Clay Johnson"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/cjohnson.jpg" alt="Staff name" /></a></li>
-				<li><a href="/who-we-are/#jhodge" rel="tooltip3" title="Jascha Franklin-Hodge"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/jhodge.jpg" alt="Staff name" /></a></li>
-				<li><a href="/who-we-are/#agreenfield" rel="tooltip3" title="Adam Greenfield"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/agreenfield.jpg" alt="Staff name" /></li>
-				<li><a href="/who-we-are/#pcorbett" rel="tooltip3" title="Peter Corbett"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/pcorbett.jpg" alt="Staff name" /></a></li>
-				<!--<li class="first"><a href="/who-we-are/#clawson" rel="tooltip3" title="Carolyn Lawson"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/clawson.jpg" alt="Staff name" /></a></li>-->
-				<li class="last"><a href="/who-we-are/#bcerveny" rel="tooltip3" title="Ben Cerveny"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/bcerveny.jpg" alt="Staff name" /></a></li>
-				<li class="first"><a href="/who-we-are/#david" rel="tooltip3"  title="David Eaves"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/david.jpg" alt="Staff name" /></a></li>
-  			
-				<li><a href="/who-we-are/#arasiej" rel="tooltip3" title="Andrew Rasiej"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/arasiej.jpg" alt="Staff name" /></a></li>
-
-				<li><a href="/who-we-are/#cshirky" rel="tooltip3" title="Clay Shirky"><img src="<?php bloginfo( 'template_url' ); ?>/images/staff/cshirky.jpg" alt="Staff name" /></a></li>
-				<li><a href="/who-we-are/#tsteinberg" rel="tooltip3" title="Tom Steinberg"><img src="http://codeforamerica.org/wp-content/uploads/2011/03/tomsteinbergsmall.jpg" width="80px" alt="Staff name" /></a></li>
-				<li><a href="/who-we-are/#ekim" rel="tooltip3" title="Eugene Eric Kim"><img src="http://codeforamerica.org/wp-content/uploads/2011/03/eugenekimsmall.jpg" width="80px" alt="Staff name" /></a></li>
-				
-				
-				
-		</ul>
-		<p align="right" style="margin-top: -3px;"><a href="/who-we-are/#city-steering" class="more"><strong>CITY STEERING COMMITTEE</strong></a>&nbsp;&nbsp;&nbsp;<a href="/jobs" class="more"><strong>JOB OPPORTUNITIES</strong></a></p>
+		<p align="right" style="margin-top: -3px;"><a href="/jobs" class="more"><strong>JOB OPPORTUNITIES</strong></a></p>
 		
 	</div>
 	
