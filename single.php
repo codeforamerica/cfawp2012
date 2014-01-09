76bde2ae790629f677fd226e611bd85da99ff1f2
Frances Berriman <fberriman@gmail.com>
2014-01-09 13:50:18 -0800

    Add columns for single entry

--- single.php
+++ single.php
@@ -28,27 +28,54 @@ get_header(); ?>
             ?>-->
 					<header class="post-header isolate">
 						<h2 class="post-title"><?php the_title(); ?></h2>
+						<time class="post-date insulate text-whisper"><?php twentyten_posted_on(); ?></time>
 					</header>
 
-					<div class="entry-meta" style="margin-top: 10px;">
-					<small>	<?php twentyten_posted_on(); ?> <?php twentyten_posted_in(); ?></small>
-					</div><!-- .entry-meta -->
+					
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+					<div class="post-body">
+                
+                    <div class="post-content layout-major">
+						<div class="entry-content">
+							<?php the_content(); ?>
+							<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
 						
-					</div>
+						</div>
 						<div class="entry-utility">
 							<!--<?php twentyten_posted_in(); ?>-->
 							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
 						</div><!-- .entry-utility --><!-- .entry-content -->
+					</div>
+						 <div class="post-author layout-minor"><!-- start author gubbins -->
+	                        <h2 class="text-whisper layout-tight">Written by</h2>
+	                        <div class="h-card">
+	                            <h3 class="p-name profile-name text-prominent">Zachary Townsend</h3>
+	                            <img class="u-photo profile-photo" src="/_media/images/avatar-zachary-townsend.jpg" />
+	                        </div>
+	                        
+	                        <div class="p-note profile-note">
+	                            <p>I have spent most of my professional life working to understand and improve the efficiencies and effectiveness of governments.</p>
+	                        </div>
+	                        
+	                        <div class="post-categories">
+	                            <h2 class="text-quiet layout-tight">Filed under STUFF</h2>
+	                            <?php twentyten_posted_in(); ?>
+	                            <!--ul class="list-no-bullets link-invert text-whisper">
+	                                <li><a href="#">Accelerator</a></li>
+	                                <li><a href="#">Commentary</a></li>
+	                                <li><a href="#">Guest Post</a></li>
+	                                <li><a href="#">Mentor</a></li>
+	                            </ul-->
+	                        </div>
+	                        
+                    	</div><!-- .post-author -->
+
 						<div id="nav-below" class="navigation" style="margin-bottom: 25px; min-height: 30px;">
-							
-						
 							<div class="nav-previous" style="width: 250px;"><?php previous_post_link( '%link', '<span class="btn">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
 							<div class="nav-next"  style="width: 250px; text-align: right;"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
 						</div>
+					
+				</div>
 
 </div>
 
