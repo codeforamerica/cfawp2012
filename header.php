63541800f2a7cab44f0ceb700ce69cbbd13fbf26
Michal Migurski <mike@teczno.com>
2014-02-14 10:22:03 -0800

    Removed Under Construction GIF

--- header.php
+++ header.php
@@ -206,7 +206,7 @@ $(document).ready(function() {
 
 	<div class="global-header">  
 	  <a href="/" class="global-header-logo">
-	      <img src="<?php bloginfo( 'template_url' ); ?>/images/logo.gif" />
+	      <img src="<?php bloginfo( 'template_url' ); ?>/images/logo.png" />
 	  </a>
 	  <p class="skip-to-nav"><a href="#global-footer">Menu</a></p>
 
